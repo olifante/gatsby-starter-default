2636c329108ed788740778712273134d02170200
anstasia <sali2ayla@gmail.com>
2017-01-31 17:40:48 +0100

    added 2 columns section

--- pages/imprint.js
+++ pages/imprint.js
@@ -17,45 +17,6 @@ import {
     Blockquote,
 } from 'rebass'
 
-const partners = [
-    {
-        filepath: '/static/partners/cnn-206b5b_f9eb2f6b61e14af381fd40d97c0a6d61~mv2.png',
-        name: 'CNN'
-    }, {
-        filepath: '/static/partners/handelsblatt-206b5b_baa1962353b54babb53ff0d431f8ae9c~mv2.png',
-        name: 'Handelsblatt'
-    }, {
-        filepath: '/static/partners/forbes-206b5b_85be8c14daff4dbc9b53e51c929aa312~mv2.png',
-        name: 'Forbes'
-    }, {
-        filepath: '/static/partners/the-guardian-206b5b_a1bff4122a704edba4305e4ca73e915c~mv2.jpg',
-        name: 'The Guardian'
-    }, {
-        filepath: '/static/partners/die-zeit-206b5b_3a6744be26374afe98174d41937dc6d6~mv2.png',
-        name: 'Die Zeit'
-    }, {
-        filepath: '/static/partners/bloomberg-206b5b_5079153fccce407985051f88d56495ef~mv2.jpg',
-        name: 'Bloomberg'
-    }, {
-        filepath: '/static/partners/washington-post-206b5b_6d848b48c8844bd597fb8345f46aa906~mv2.jpg',
-        name: 'Washington Post'
-    }, {
-        filepath: '/static/partners/welt-n24-206b5b_7ee4cf0f2ead4fa8a7b804dbf7f202c7~mv2.png',
-        name: 'Die Welt'
-    }, {
-        filepath: '/static/partners/ajplus-206b5b_76f5324b0a1d4481bf0dbdd5e7254c26~mv2.png',
-        name: 'AJ+'
-    }, {
-        filepath: '/static/partners/tedx-innovations-206b5b_009aec41271c410bb79c07c2820c2f70~mv2.jpg',
-        name: 'TEDx ,Innovations'
-    }, {
-        filepath: '/static/partners/co-exist-206b5b_981ef7b40f764642ad396d71646e0df8~mv2.png',
-        name: 'Co-exist'
-    }, {
-        filepath: '/static/partners/deutsche-welle-206b5b_05500378376d46f0bb3342c0b0a12ad8~mv2.jpg',
-        name: 'Deutsche Welle'
-    },
-]
 
 export default () => (
     <div>
@@ -91,29 +52,88 @@ export default () => (
                     />
             </NavItem>
         </Toolbar>
-       <Container m={1} backgroundColor='#CDE6ED'>
-                <Heading
-                    level={1}
-                    big={true}
-                    children='Contact & Imprint'
-                    />
-                <Text
-                    size={3}
-                    children='We are looking forward to hearing from you.'
-                    />
-                <ButtonOutline
-                    children='CONTACT US'
-                    color="black"
-                    />
-               
-            </Container>
-          <Banner
+        <Container m={1} backgroundColor='#CDE6ED'>
+            <Heading
+                level={1}
+                big={true}
+                children='Contact & Imprint'
+                />
+            <Text
+                size={3}
+                children='We are looking forward to hearing from you.'
+                />
+            <ButtonOutline
+                children='CONTACT US'
+                color="black"
+                />
+
+        </Container>
+        <Banner
             backgroundImage='/static/banners/206b5b_681e6b87fe394df28148acd6e86ec8c8~mv2_d_7360_4912_s_4_2.jpg'
             >
-
-               
         </Banner>
-   
+
+        <Flex
+            wrap
+            justify='space-between'
+            >
+            <Flex
+                column
+                style={{
+                    width: 320,
+                    minHeight: 384,
+                }}
+                >
+                <Space auto />
+                <Heading level={3} >
+                    ReDI School of Digital Integration gGmbH
+                </Heading>
+                <Space auto />
+                <Text>
+                    Lückhoffstr. 24
+                    <br />
+                    14129 Berlin
+                    <br />
+                    Germany
+                    <br />
+                    Phone.: +49 (0)176 98 23 33 42
+                    <br />
+                    E-Mail: hello@redi-school.org
+                    <br />
+                    Online: www.redi-school.org
+                    <br />
+                    <br />
+                    Managing director: Anne Kjaer Riechert
+                    <br />
+                    Commercial Register: Amtsgericht Berlin Charlottenburg HRB: 174213
+                    <br />
+                    Registered Office: Berlin
+                    <br />
+                    USt-IdNr.: DE304512647
+                    <br />
+                    <br />
+                    Visiting address:
+                    <br />
+                    <br />
+                    hub:raum, Winterfeldtstraße 21,
+                    <br />
+                    10781 Berlin, Germany - See map
+​                    <br />
+                    <br />
+                    * ReDI School of Digital Integration gGmbH does not take any responsibility for the content of linked pages.
+                </Text>
+                <Space auto />
+                <Text>
+                    Our aim is to provide our students with valuable digital skills and a strong network of tech leaders, students and alumni to help create new opportunities for all.
+                </Text>
+                <Space auto />
+            </Flex>
+            <Flex column>
+                <img src="/static/206b5b_09a912e25ea14dddbb1673f21488b5bd~mv2.png" />
+            </Flex>
+        </Flex>
+
+
         <Footer m={2} >
             <NavItem
                 small={true}
